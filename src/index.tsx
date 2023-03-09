84dd0cf51503eac45d42d9fd0f9af00bacf769aa
Wanyaxeli <eliwanyax@gmail.com>
2023-03-09 14:22:24 +0300

    added baseline

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-   <BrowserRouter basename={process.env.PUBLIC_URL}>
+   <BrowserRouter>
    <App />
    </BrowserRouter>
   </React.StrictMode>
